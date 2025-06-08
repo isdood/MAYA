516aa931cd701deeb7a3c60204bdbd1350f72b19
isdood <calebjdt@proton.me>
2025-06-08 00:34:29 -0400

    Debugging compilation errors - doesn't currently build 🌟

--- src/renderer/vulkan.zig
+++ src/renderer/vulkan.zig
@@ -157,7 +157,7 @@ pub const VulkanRenderer = struct {
 
     fn createSurface(self: *Self, window: *glfw.GLFWwindow) !void {
         const result = glfw.glfwCreateWindowSurface(
-            self.instance,
+            @ptrCast(vk.VkInstance, self.instance),
             window,
             null,
             &self.surface,
